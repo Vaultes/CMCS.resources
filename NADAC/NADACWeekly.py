40518eace52e506520b6dbda95f292736b5016ef
corp-myer-vaultes <christopher.corp-myer@vaultes.com>
2026-05-19 09:05:06 -0700

    NADAC Weekly and Comparison Scripts (#1)
    * Initial commit for AMP and NADAC scripts and workplace setup
    
    * Updated scripts and README files

---
+++ NADAC/NADACWeekly.py
@@ -0,0 +1,108 @@
+import argparse
+from collections import defaultdict
+import pathlib
+import pandas
+
+# Command line argument parsing
+parser = argparse.ArgumentParser()
+
+# Script Arguments:
+#   - NADAC .xlsx file
+#   - Production CSV file
+#   - As of Date in YYYYMMDD format
+parser.add_argument("-i","--input", help="Path to the Weekly NADAC Excel (.xlsx) file", type=pathlib.Path, required=True) 
+parser.add_argument("-p","--prodExport", help="Path to the exported production CSV file", type=pathlib.Path, required=True) 
+parser.add_argument("-d","--asofdate", help="As of Date in MM/DD/YYYY format", type=str, required=True)
+
+args = parser.parse_args()
+
+# Sanity Check
+print("Parsing input file: ", args.input)
+print("Parsing production export file: ", args.prodExport)
+print("Using As of Date: ", args.asofdate)
+
+# Check if the date format is correct for the As of Date. We expect MM/DD/YYYY.
+try:
+    pandas.to_datetime(args.asofdate, format='%m/%d/%Y')
+except ValueError:
+    print("**** Error: As of Date is not in the correct format. Please use MM/DD/YYYY.")
+    exit(1)
+
+# Check the files exist before we attempt to read them.
+inputExists = args.input.is_file()
+prodExportExists = args.prodExport.is_file()
+
+if not inputExists:
+    print("**** Error: Input file does not exist.")
+    exit(1)
+
+if not prodExportExists:
+    print("**** Error: Production export file does not exist.")
+    exit(1)
+
+# As the parameter is required we should always have a valid path.
+#   We skip the top three rows as they're the title, and two blank rows.
+inputDataFrame = pandas.read_excel(args.input, skiprows=3)
+
+# Load the production exported CSV. Load all columns as a string 'str'.
+dtype_dict_str = defaultdict(lambda: 'str')
+prodDataFrame = pandas.read_csv(args.prodExport, dtype=dtype_dict_str)
+
+# Initial row counts
+print("Input file contains ", len(inputDataFrame), " rows.")
+print("Production export file contains ", len(prodDataFrame), " rows.")
+
+inputRowCount = len(inputDataFrame)
+prodRowCount = len(prodDataFrame)
+expectedRowCount = inputRowCount + prodRowCount
+
+# Input .xlsx columns need to be converted to match the columns in the production export.
+inputDataFrame.rename({
+    'NADAC\nPer Unit':'NADAC Per Unit',
+    'Effective\nDate':'Effective Date',
+    'Pricing\nUnit':'Pricing Unit',
+    'Pharmacy\nType\nIndicator':'Pharmacy Type Indicator',
+    'Explanation\nCode':'Explanation Code',
+    'Classification\nfor Rate\nSetting':'Classification for Rate Setting',
+    'Corresponding\nGeneric Drug\nNADAC\nPer Unit':'Corresponding Generic Drug NADAC Per Unit',
+    'Corresponding\nGeneric Drug\nEffective\nDate':'Corresponding Generic Drug Effective Date'}, 
+    axis=1, inplace=True)
+
+# Add a new column to the inputDataFrame for the "As of Date" using the value provided in the command line argument.
+# Format the "As of Date" to MM/DD/YYYY inline with the Data Dictionary specifications.
+inputDataFrame.insert(11, "As of Date", args.asofdate)
+
+# Combine the new file and production export.
+combinedDataFrame = pandas.concat([prodDataFrame, inputDataFrame], ignore_index=True)
+combinedRowCount = len(combinedDataFrame)
+
+# Apply column formatting inline with the Data Dictionary specifications:
+# NDC: 11 characters, padding with leading zeros as needed.
+combinedDataFrame['NDC'] = combinedDataFrame['NDC'].astype(str)
+combinedDataFrame['NDC'] = combinedDataFrame['NDC'].str.rjust(11, "0")
+
+# NADAC Per Unit: 5 decimal places.
+combinedDataFrame['NADAC Per Unit'] = combinedDataFrame['NADAC Per Unit'].astype(float)
+combinedDataFrame['NADAC Per Unit'] = combinedDataFrame["NADAC Per Unit"].map('{:.5f}'.format)
+
+# Corresponding Generic Drug Effective Date: Format to MM/DD/YYYY.
+combinedDataFrame["Corresponding Generic Drug Effective Date"] = \
+    pandas.to_datetime(combinedDataFrame['Corresponding Generic Drug Effective Date'])
+combinedDataFrame["Corresponding Generic Drug Effective Date"] = \
+    combinedDataFrame["Corresponding Generic Drug Effective Date"].dt.strftime('%m/%d/%Y')
+
+# Effective Date: Format to MM/DD/YYYY.
+combinedDataFrame['Effective Date'] = pandas.to_datetime(combinedDataFrame['Effective Date'])
+combinedDataFrame['Effective Date'] = combinedDataFrame['Effective Date'].dt.strftime('%m/%d/%Y')
+
+# As of Date: Format to MM/DD/YYYY.
+combinedDataFrame['As of Date'] = pandas.to_datetime(combinedDataFrame['As of Date'])
+combinedDataFrame['As of Date'] = combinedDataFrame['As of Date'].dt.strftime('%m/%d/%Y')
+
+# Output combined file
+combinedDataFrame.to_csv('NADAC_Weekly_Combined_File.csv', index=False)
+
+# Output Results
+print("Processing complete.")
+print("Combined Output: ", combinedRowCount, " rows written to NADAC_Weekly_Combined_File.csv")
+print("Expected Row Count: ", expectedRowCount, " Received: ", combinedRowCount, " Difference: ", expectedRowCount - combinedRowCount)
---
+++ NADAC/NADACWeeklyComparison.py
@@ -0,0 +1,109 @@
+import argparse
+from collections import defaultdict
+import pathlib
+import pandas
+
+# Command line argument parsing
+parser = argparse.ArgumentParser()
+
+# Script Arguments:
+#   - NADAC .xlsx file
+#   - Production CSV file
+#   - As of Date in YYYYMMDD format
+parser.add_argument("-i","--input", help="Path to the NADAC Comparison Excel (.xlsx) file", type=pathlib.Path, required=True) 
+parser.add_argument("-p","--prodExport", help="Path to the exported production CSV file", type=pathlib.Path, required=True) 
+parser.add_argument("-sd","--startdate", help="Start Date in MM/DD/YYYY format", type=str, required=True)
+parser.add_argument("-ed","--enddate", help="End Date in MM/DD/YYYY format", type=str, required=True)
+
+args = parser.parse_args()
+
+# Sanity Check
+print("Parsing input file: ", args.input)
+print("Parsing production export file: ", args.prodExport)
+print("Using start date: ", args.startdate)
+print("Using end date: ", args.enddate)
+
+# Check if the date format is correct for the Start Date. We expect MM/DD/YYYY.
+try:
+    pandas.to_datetime(args.startdate, format='%m/%d/%Y')
+except ValueError:
+    print("**** Error: Start Date is not in the correct format. Please use MM/DD/YYYY.")
+    exit(1)
+
+# Check if the date format is correct for the End Date. We expect MM/DD/YYYY.
+try:
+    pandas.to_datetime(args.enddate, format='%m/%d/%Y')
+except ValueError:
+    print("**** Error: End Date is not in the correct format. Please use MM/DD/YYYY.")
+    exit(1)
+
+# Check the files exist before we attempt to read them.
+inputExists = args.input.is_file()
+prodExportExists = args.prodExport.is_file()
+
+if not inputExists:
+    print("**** Error: Input file does not exist.")
+    exit(1)
+
+if not prodExportExists:
+    print("**** Error: Production export file does not exist.")
+    exit(1)
+
+# Load the comparison .xlsx file. Skip the top five rows as they're the title, and four blank rows.
+inputDataFrame = pandas.read_excel(args.input, skiprows=5)
+
+# Load the production exported CSV. Load all columns as a string 'str'.
+dtype_dict_str = defaultdict(lambda: 'str')
+prodDataFrame = pandas.read_csv(args.prodExport, dtype=dtype_dict_str)
+
+# Initial row counts
+print("Input file contains ", len(inputDataFrame), " rows.")
+print("Production export file contains ", len(prodDataFrame), " rows.")
+
+inputRowCount = len(inputDataFrame)
+prodRowCount = len(prodDataFrame)
+expectedRowCount = inputRowCount + prodRowCount
+
+# Input .xlsx columns need to be converted to match the columns in the production export.
+inputDataFrame.rename({
+    'Old NADAC\nPer Unit':'Old NADAC Per Unit',
+    'New NADAC\nPer Unit':'New NADAC Per Unit',
+    'New NADAC\nEffective\nDate':'Effective Date'
+    }, axis=1, inplace=True)
+
+# Add new columns for the Start Date and End Date using the values provided in the command line arguments.
+inputDataFrame.insert(7, "Start Date", args.startdate)
+inputDataFrame.insert(8, "End Date", args.enddate)
+
+# Reorder the onput columns to match the expected output format.
+Column_Name = ['NDC Description','NDC','Old NADAC Per Unit','New NADAC Per Unit','Classification for Rate Setting','Percent Change','Primary Reason','Start Date','End Date','Effective Date']
+inputDataFrame = inputDataFrame.reindex(columns=Column_Name)
+
+# Combine the new file and production export.
+combinedDataFrame = pandas.concat([prodDataFrame, inputDataFrame], ignore_index=True)
+combinedRowCount = len(combinedDataFrame)
+
+combinedDataFrame['NDC'] = combinedDataFrame['NDC'].astype(str)
+combinedDataFrame['NDC'] = combinedDataFrame['NDC'].str.rjust(11, "0")
+
+combinedDataFrame['Percent Change'] = combinedDataFrame['Percent Change'].astype(float)
+combinedDataFrame['Percent Change'] = combinedDataFrame['Percent Change']*100
+combinedDataFrame['Percent Change'] = combinedDataFrame["Percent Change"].map('{:.2f}'.format)
+
+combinedDataFrame['Old NADAC Per Unit'] = combinedDataFrame['Old NADAC Per Unit'].astype(float)
+combinedDataFrame['Old NADAC Per Unit'] = combinedDataFrame["Old NADAC Per Unit"].map('{:.5f}'.format)
+
+combinedDataFrame['New NADAC Per Unit'] = combinedDataFrame['New NADAC Per Unit'].astype(float)
+combinedDataFrame['New NADAC Per Unit'] = combinedDataFrame["New NADAC Per Unit"].map('{:.5f}'.format)
+
+combinedDataFrame['Start Date'] = pandas.to_datetime(combinedDataFrame['Start Date'], format='%m/%d/%Y').dt.strftime('%m/%d/%Y')
+combinedDataFrame['End Date'] = pandas.to_datetime(combinedDataFrame['End Date'], format='%m/%d/%Y').dt.strftime('%m/%d/%Y')
+combinedDataFrame["Effective Date"] = pandas.to_datetime(combinedDataFrame["Effective Date"], format='%m/%d/%Y').dt.strftime('%m/%d/%Y')
+
+# Export the combined and cormatted file
+combinedDataFrame.to_csv('NADAC_Weekly_Combined_File.csv', index=False)
+
+# Output Results
+print("Processing complete.")
+print("Combined Output: ", combinedRowCount, " rows written to NADAC_Weekly_Combined_File.csv")
+print("Expected Row Count: ", expectedRowCount, " Received: ", combinedRowCount, " Difference: ", expectedRowCount - combinedRowCount)
