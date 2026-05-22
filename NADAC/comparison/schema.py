e32fc88695d6c1d0ede48dd3b2f07884d43127a2
Christopher Corp-Myer <christopher.corp-myer@vaultes.com>
2026-05-22 10:15:33 -0700

    Schema appears to work. Need to confirm Primary Reason literals

--- NADAC/comparison/schema.py
+++ NADAC/comparison/schema.py
@@ -15,12 +15,28 @@ class NADACComparisonSchema(BaseModel):
     Old_NADAC_Per_Unit: float = Field(alias="Old NADAC Per Unit")
     New_NADAC_Per_Unit: float = Field(alias="New NADAC Per Unit")
     Classification_for_Rate_Setting: str = Field(alias="Classification for Rate Setting")
-    Percent_Change: str = Field(alias="Percent Change")
-    Primary_Reason: str = Field(alias="Primary Reason")
+    Percent_Change: float = Field(alias="Percent Change")
+    Primary_Reason: Literal["Survey Rate", "WAC Adjustment", "Help Desk"] = Field(alias="Primary Reason")
     Start_Date: date = Field(alias="Start Date")
     End_Date: date = Field(alias="End Date")
     Effective_Date: date = Field(alias="Effective Date")
-
+    
+    @field_validator("New_NADAC_Per_Unit", "Old_NADAC_Per_Unit")
+    def five_decimal_places(cls, v: float) -> float:
+        if v is None:
+            return v
+        if round(v, 5) != v:
+            raise ValueError("Field must have 5 decimal places")
+        return v
+    
+    @field_validator("Percent_Change")
+    def two_decimal_places(cls, v: float) -> float:
+        if v is None:
+            return v
+        if round(v, 2) != v:
+            raise ValueError("Field must have 2 decimal places")
+        return v
+    
     @field_validator('Start_Date', 'End_Date', 'Effective_Date', mode='before')
     def parse_custom_date(cls, v):
         if isinstance(v, float) and math.isnan(v):
--- Shared/ValidateDataFrame.py
+++ Shared/ValidateDataFrame.py
@@ -1,9 +1,15 @@
 from pandantic import Pandantic
 import pandas
 
+log = False
+
 # Validation logic using pandantic
 def validateDataFrame(df: pandas.DataFrame, targetSchema):
     validator = Pandantic(schema=targetSchema)
+
+    if log:
+        invalid = validator.validate(df, errors='log')
+        print(invalid)
 
     # Validate the DataFrame against the schema
     try:
