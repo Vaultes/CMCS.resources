d1fcfa1fe9bd2589b9d163c8e49f23b40b6e494c
corp-myer-vaultes <christopher.corp-myer@vaultes.com>
2026-05-27 11:08:13 -0700

    AMP Monthly script and schema (#7)

---
+++ AMP/monthly/schema.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+class AMPMonthlySchema(BaseModel):
+    # Forbid extra fields not defined in the schema
+    model_config = {
+        "extra": "forbid",
+    }
+    # Define each column. Headers with spaces or special characters are handled using the Field alias.
+    Labeler_Name: str = Field(alias="Labeler Name")
+    NDC: str = Field(min_length=11, max_length=11)
+    FDA_Product_Name: str = Field(alias="FDA Product Name")
+    Status: str = Field(alias="Status")
+    Year: str = Field(alias="Year", min_length=4, max_length=4)
+    Month: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"] = Field(alias="Month")
--- AMPMonthlyImport.py
+++ AMPMonthlyImport.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 import argparse
+from collections import defaultdict
+from datetime import datetime
 import pathlib
 import pandas
+
+from AMP.monthly.schema import AMPMonthlySchema
+from Shared.ValidateDataFrame import validateDataFrame
+
+startTime = datetime.now()
+print("Script started at: ", startTime)
 
 # Command line argument parsing
 parser = argparse.ArgumentParser()
@@ -17,6 +25,32 @@ parser.add_argument("-m","--month", help="Current month in MM format", type=str,
 
 args = parser.parse_args()
 
+# Verify the year format.
+try:
+    pandas.to_datetime(args.year, format='%Y')
+except ValueError:
+    print("**** Error: Year is not in the correct format. Please use YYYY.")
+    exit(1)
+
+# Verify the month format.
+try:
+    pandas.to_datetime(args.month, format='%m')
+except ValueError:
+    print("**** Error: Month is not in the correct format. Please use MM.")
+    exit(1)
+
+# Check the files exist before we attempt to read them.
+inputExists = args.input.is_file()
+prodExportExists = args.prodExport.is_file()
+
+if not inputExists:
+    print("**** Error: Input file does not exist.")
+    exit(1)
+
+if not prodExportExists:
+    print("**** Error: Production export file does not exist.")
+    exit(1)
+
 # Sanity Check
 print("Parsing input file: ", args.input)
 print("Parsing production export file: ", args.prodExport)
@@ -24,16 +58,47 @@ print("Using Year: ", args.year)
 print("Using Month: ", args.month)
 
 # Read Excel 
-df = pandas.read_excel(args.input, skiprows = 4)
-df['NDC-11'] = df['NDC-11'].astype(str)
-df['NDC-11'] = df['NDC-11'].str.rjust(11, "0")
+inputDataFrame = pandas.read_excel(args.input, skiprows = 4)
 
-df.rename({'NDC-11':'NDC', 'Product Name':'FDA Product Name', 'AMP':'Status'}, axis = 1, inplace = True)
+# Load the production exported CSV. Load all columns as a string 'str'.
+dtype_dict_str = defaultdict(lambda: 'str')
+prodDataFrame = pandas.read_csv(args.prodExport, dtype=dtype_dict_str)
+
+# Initial row counts
+print("Input file contains ", len(inputDataFrame), " rows.")
+print("Production export file contains ", len(prodDataFrame), " rows.")
+
+inputRowCount = len(inputDataFrame)
+prodRowCount = len(prodDataFrame)
+expectedRowCount = inputRowCount + prodRowCount
+
+inputDataFrame['NDC-11'] = inputDataFrame['NDC-11'].astype(str)
+
+inputDataFrame.rename({'NDC-11':'NDC', 'Product Name':'FDA Product Name', 'AMP':'Status'}, axis = 1, inplace = True)
 
 # Add month and year column  
-df['Year'] = int(args.year)
-df['Month'] = int(args.month)
-df.head()
+inputDataFrame['Year'] = int(args.year)
+inputDataFrame['Month'] = int(args.month)
+inputDataFrame.head()
+
+# Combine the new file and production export.
+combinedDataFrame = pandas.concat([prodDataFrame, inputDataFrame], ignore_index=True)
+combinedDataFrame['NDC'] = combinedDataFrame['NDC'].str.rjust(11, "0")
+combinedDataFrame['Month'] = combinedDataFrame['Month'].astype(str)
+combinedDataFrame['Year'] = combinedDataFrame['Year'].astype(str)
+
+combinedRowCount = len(combinedDataFrame)
+
+validateDataFrame(combinedDataFrame, AMPMonthlySchema)
 
 # Export as CSV
-df.to_csv('DrugAMPReportingMonthly.csv', index=False, encoding = 'utf-8-sig')
+combinedDataFrame.to_csv('AMP_Monthly_Combined_File.csv', index=False, encoding = 'utf-8-sig')
+
+# Output Results
+print("Processing complete.")
+print("Combined Output: ", combinedRowCount, " rows written to AMP_Monthly_Combined_File.csv")
+print("Expected Row Count: ", expectedRowCount, " Received: ", combinedRowCount, " Difference: ", expectedRowCount - combinedRowCount)
+
+endTime = datetime.now()
+print("Script finished at: ", endTime)
+print("Total processing time: ", endTime - startTime)
